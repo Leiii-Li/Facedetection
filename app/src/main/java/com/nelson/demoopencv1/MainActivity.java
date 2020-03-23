76d1115a6dcb31feab933715dccb72bd054e5bc6
Nelson <lilei@vsoontech.com>
2020-03-23 16:49:50 +0800

    可以运行以及看到结果数据

--- app/src/main/java/com/nelson/demoopencv1/MainActivity.java
+++ app/src/main/java/com/nelson/demoopencv1/MainActivity.java
@@ -1,29 +1,65 @@
 package com.nelson.demoopencv1;
 
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.hardware.Camera.PreviewCallback;
+import android.os.Environment;
+import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
+import android.view.SurfaceView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import com.nelson.demoopencv1.helper.CameraHelper;
+import com.nelson.demoopencv1.helper.Utils;
+import java.io.File;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Callback, PreviewCallback {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+    private OpencvHelper mOpencvHelper;
+    private CameraHelper mCameraHelper;
+    int cameraId = CameraInfo.CAMERA_FACING_BACK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        mOpencvHelper = new OpencvHelper();
+        SurfaceView surfaceView = findViewById(R.id.surfaceView);
+        surfaceView.getHolder().addCallback(this);
+        mCameraHelper = new CameraHelper(cameraId);
+        mCameraHelper.setPreviewCallback(this);
+
+        Utils.copyAssets(this, "lbpcascade_frontalface.xml");
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String path = new File(Environment.getExternalStorageDirectory(), "lbpcascade_frontalface.xml")
+            .getAbsolutePath();
+        mCameraHelper.startPreview();
+        mOpencvHelper.init(path);
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        mOpencvHelper.setSurface(holder.getSurface());
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void onPreviewFrame(byte[] data, Camera camera) {
+        mOpencvHelper.postData(data, CameraHelper.WIDTH, CameraHelper.HEIGHT, cameraId);
+    }
 }
